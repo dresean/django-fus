11991d360771fbf103fffbbbd9d8956735523082
Dre'Sean Williams <williamsdresean@gmail.com>
2018-09-15 16:59:03 -0400

    add view and url plus settings setup

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
+def index(request):
+  return HttpResponse("Hello world, you're at the index of the polls app!")
 # Create your views here.
