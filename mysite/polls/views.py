9ae09b7bbd7b918cf66ac84f76ec28652eb99d46
Dre'Sean Williams <williamsdresean@gmail.com>
2018-09-16 19:24:01 -0400

    added custom view, template, and url for choices

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,9 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, Http404
 from django.template import loader
 # to call the templates in our polls/templates/polls/index.html file w/o render shortcut
 
-from .models import Question
+from .models import Question, Choice
 
 def index(request):
   latest_question_list = Question.objects.order_by('-pub_date')[:5]
@@ -14,11 +14,8 @@ def index(request):
   return render(request,'polls/index.html', context,)
 # Create your views here.
 def detail(request, question_id):
-  try:
-    question = Question.objects.get(pk=question_id)
-  except Question.DoesNotExist:
-    raise Http404("Question does not exist")
-  return render(request, '/polls/detail.html', {'question': question})
+  question = get_object_or_404(Question, pk=question_id)
+  return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
   response = f" you're looking at the results from question {question_id}"
@@ -27,3 +24,9 @@ def results(request, question_id):
 def vote(request, question_id): 
   response = f" You're voting on question {question_id}"
   return HttpResponse(response)
+
+def choices(request, question_id):
+
+  choices = get_object_or_404(Choice, id=question_id)
+  question = get_object_or_404(Question, pk=question_id)
+  return render(request, 'polls/choices.html' , {'question': question, 'choices': choices})
